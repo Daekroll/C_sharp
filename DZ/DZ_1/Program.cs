48b6aae4977214b20f314a8662de5573d59acbe5
igor <vavilonskiy99@mail.ru>
2023-10-12 18:51:24 +0500

    Семинар 1 + дз

--- DZ/DZ_1/Program.cs
+++ DZ/DZ_1/Program.cs
@@ -3,15 +3,41 @@
 // Console.WriteLine("Введите второе число");
 // int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-if (firstNumber > secondNumber)
-{
-    Console.WriteLine("Первое число " + " `" + firstNumber + "` больше чем второе число `" + secondNumber + "`");
-}
-else if (firstNumber < secondNumber)
-{
-    Console.WriteLine("Первое число `" + firstNumber + "` меньше чем второе число `" + secondNumber + "`");
-}
-else
-{
-    Console.WriteLine("Введенные числа равны `" + firstNumber + "`");
-}
+// if (firstNumber > secondNumber)
+// {
+//     Console.WriteLine("Первое число " + " `" + firstNumber + "` больше чем второе число `" + secondNumber + "`");
+// }
+// else if (firstNumber < secondNumber)
+// {
+//     Console.WriteLine("Первое число `" + firstNumber + "` меньше чем второе число `" + secondNumber + "`");
+// }
+// else
+// {
+//     Console.WriteLine("Введенные числа равны `" + firstNumber + "`");
+// }
+
+
+int a = 9;
+int b = 9;
+int c = 5;
+int result = 0;
+    if (a > b)
+    {
+      if (a > c)
+      {
+        result = a;
+      }
+        else
+      {
+        result = c;
+      }
+    }
+    else if (b > c)
+    {
+      result = b;
+    }
+      else
+    {
+        result = c;
+    }
+Console.Write(result);
