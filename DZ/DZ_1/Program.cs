9f534c3c6cb27b106e69c8c89c1e4abcc0d329d2
igor <vavilonskiy99@mail.ru>
2023-10-12 16:00:06 +0500

    Первое дз

---
+++ DZ/DZ_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Console.WriteLine("Введите первое число");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine("Первое число " + " `" + firstNumber + "` больше чем второе число `" + secondNumber + "`");
+}
+else if (firstNumber < secondNumber)
+{
+    Console.WriteLine("Первое число `" + firstNumber + "` меньше чем второе число `" + secondNumber + "`");
+}
+else
+{
+    Console.WriteLine("Введенные числа равны `" + firstNumber + "`");
+}
