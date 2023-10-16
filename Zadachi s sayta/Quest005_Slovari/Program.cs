814a451c7d44145c2bb72b3044cc5e9e966a866b
igor <vavilonskiy99@mail.ru>
2023-10-17 00:28:10 +0500

    add slovar'

---
+++ Zadachi s sayta/Quest005_Slovari/Program.cs
@@ -0,0 +1,29 @@
+﻿int num = new Random().Next(14);
+Console.WriteLine(num);
+var Week = new Dictionary<int, string>()
+{
+    {1, "Понедельник"},
+    {2, "Вторник"},
+    {3, "Среда"},
+    {4, "Четверг"},
+    {5, "Пятница"},
+    {6, "Субботу"},
+    {7, "Воскресенье"},
+};
+
+bool day = Week.ContainsKey(num);
+if (day)
+{
+    if(Week[num] == "Субботу" || Week[num] == "Воскресенье")
+    {
+        Console.WriteLine("Урааа выходной!!!");
+    }
+    else
+    {
+        Console.WriteLine("Поработаем еще");
+    }
+}
+else
+{
+    Console.WriteLine("Это не день недели");
+}
