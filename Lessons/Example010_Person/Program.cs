97ad04e8189c7f6e913c3b097dd75e4d0fc73e3c
igor <vavilonskiy99@mail.ru>
2023-10-17 00:05:28 +0500

    add classes

---
+++ Lessons/Example010_Person/Program.cs
@@ -0,0 +1,4 @@
+﻿Person John = new Person("John", 34);
+string Name = name;
+int Age = age;
+Console.WriteLine($"{0}s age is {1}", John.name, John.age);
--- Zadachi s sayta/Quest002/Program.cs
+++ Zadachi s sayta/Quest002/Program.cs
@@ -82,19 +82,19 @@ public class Application
         // Create  struct instance and initialize by using "new".
         // Memory is allocated on thread stack.
         Person p1 = new Person("Alex", 9);
-        Console.WriteLine("p1 Name = {0} Age = {1}", p1.Name, p1.Age);
+        Console.WriteLine("{0}s age is = {1}", p1.Name, p1.Age);
 
-        // Create  new struct object. Note that  struct can be initialized
-        // without using "new".
-        Person p2 = p1;
+        // // Create  new struct object. Note that  struct can be initialized
+        // // without using "new".
+        // Person p2 = p1;
 
-        // Assign values to p2 members.
-        p2.Name = "Spencer";
-        p2.Age = 7;
-        Console.WriteLine("p2 Name = {0} Age = {1}", p2.Name, p2.Age);
+        // // Assign values to p2 members.
+        // p2.Name = "Spencer";
+        // p2.Age = 7;
+        // Console.WriteLine("p2 Name = {0} Age = {1}", p2.Name, p2.Age);
 
-        // p1 values remain unchanged because p2 is  copy.
-        Console.WriteLine("p1 Name = {0} Age = {1}", p1.Name, p1.Age);
+        // // p1 values remain unchanged because p2 is  copy.
+        // Console.WriteLine("p1 Name = {0} Age = {1}", p1.Name, p1.Age);
     }
 }
 /*
