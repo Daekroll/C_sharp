eaf120b8e2b0eeeadbd22d31210321f553b54bce
igor <vavilonskiy99@mail.ru>
2023-10-17 22:55:15 +0500

    add substring

--- Zadachi s sayta/Quest005_Slovari/Program.cs
+++ Zadachi s sayta/Quest005_Slovari/Program.cs
@@ -1,29 +1,33 @@
-﻿int num = new Random().Next(14);
-Console.WriteLine(num);
-var Week = new Dictionary<int, string>()
-{
-    {1, "Понедельник"},
-    {2, "Вторник"},
-    {3, "Среда"},
-    {4, "Четверг"},
-    {5, "Пятница"},
-    {6, "Субботу"},
-    {7, "Воскресенье"},
-};
+﻿// int num = new Random().Next(14);
+// Console.WriteLine(num);
+// var Week = new Dictionary<int, string>()
+// {
+//     {1, "Понедельник"},
+//     {2, "Вторник"},
+//     {3, "Среда"},
+//     {4, "Четверг"},
+//     {5, "Пятница"},
+//     {6, "Субботу"},
+//     {7, "Воскресенье"},
+// };
 
-bool day = Week.ContainsKey(num);
-if (day)
-{
-    if(Week[num] == "Субботу" || Week[num] == "Воскресенье")
-    {
-        Console.WriteLine("Урааа выходной!!!");
-    }
-    else
-    {
-        Console.WriteLine("Поработаем еще");
-    }
-}
-else
-{
-    Console.WriteLine("Это не день недели");
-}
+// bool day = Week.ContainsKey(num);
+// if (day)
+// {
+//     if(Week[num] == "Субботу" || Week[num] == "Воскресенье")
+//     {
+//         Console.WriteLine("Урааа выходной!!!");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Поработаем еще");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Это не день недели");
+// }
+
+string str = "abcdefg";
+
+System.Console.WriteLine(str.Substring(1, str.Length - 2));
