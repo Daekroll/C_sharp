5b668144646421cc0b9df4d84bfb1768af104684
igor <vavilonskiy99@mail.ru>
2023-10-23 13:07:59 +0500

    add seminar 3 and dz 3

---
+++ DZ/DZ_3/Program.cs
@@ -0,0 +1,63 @@
+﻿// // Задание 1
+
+
+// System.Console.WriteLine("Введите число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine("Введите степень");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// double PowNumber1(int a, int b)
+// {
+//     return Math.Pow(a, b);
+// }
+
+// double PowNumber2(int a, int b)
+// {
+//     int result = 1;
+//     for (int i = 0; i < b; i++)
+//     {
+//         result*=a;
+//     }
+//     return result;
+// }
+
+// System.Console.WriteLine(PowNumber1(num1,num2));
+// System.Console.WriteLine(PowNumber2(num1,num2));
+
+// Задание 2
+
+// System.Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int SumCharNumber(int a)
+// {
+//     int result = 0;
+//     while (a > 0)
+//     {
+//         result+=a%10;
+//         a/=10;
+//     }
+//     return result;
+// }
+
+// System.Console.WriteLine(SumCharNumber(num));
+
+
+// Задание 3
+
+
+// System.Console.WriteLine("Введите размер массива");
+// int range = Convert.ToInt32(Console.ReadLine());
+
+// void CreateArray(int a)
+// {
+//     int[] arr = new int[a];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(0, 50);
+//         Console.Write($"{arr[i]} ");
+//     }
+// }
+
+// CreateArray(range);
---
+++ Seminars/Seminar 4/Program.cs
@@ -0,0 +1,62 @@
+﻿// System.Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int Sum(int A)
+// {
+//     int result = 0;
+//     for (int i = 1; i <= A; i++)
+//     {
+//         result+=i;
+//     }
+//     return result;
+// }
+// int res = Sum(num);
+// System.Console.WriteLine(res);
+
+// System.Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int GetCount(int A)
+// {
+//     int count = 0;
+//     int result = A;
+//     while (result > 0)
+//     {
+//         result = result / 10;
+//         count++;
+//     }
+//     return count;
+// }
+
+// int res = GetCount(num);
+// System.Console.WriteLine(res);
+
+
+
+// System.Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int Pos(int A)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= A; i++)
+//     {
+//         result = result*i;
+//     }
+//     return result;
+// }
+// int res = Pos(num);
+// System.Console.WriteLine(res);
+
+
+void CreateArray()
+{
+    int[] arr = new int[8];
+    int i = 0;
+    while (i < arr.Length)
+    {
+        arr[i] = new Random().Next(0,2);
+        System.Console.Write($"{arr[i]} ");
+        i++;
+    }
+}
+
+CreateArray();
