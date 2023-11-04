f721cd8cb7793417a615f0db079e5e98d2b56b29
igor <vavilonskiy99@mail.ru>
2023-11-05 03:14:19 +0500

    порешал задачи

--- Zadachi s sayta/Quest005_Slovari/Program.cs
+++ Zadachi s sayta/Quest005_Slovari/Program.cs
@@ -95,3 +95,57 @@
 // }
 // Digitize(n);
 // System.Console.WriteLine(n);
+
+// int[] numbers = new int[] {1, 2, 5, 16, 60, 3};
+
+
+
+// int[][] data = new[] { new int[] { 45, 12 }, new int[] { 55, 21 }, new int[] { 19, 2 }, new int[] { 104, 20 }};
+// string opn = "Open";
+// string cls = "Senior";
+// string[] member = new string[data.Length];
+// for (int i = 0; i < data.Length; i++)
+//     {
+        
+//         if (data[i][0] >= 55 && data[i][1] >= 7) member[i] = cls;
+//          else member[i] = opn;
+//     }
+ 
+
+// void Print(string[] array)
+//     {
+//         for (int i = 0; i < array.Length; i++)
+//         {
+//             System.Console.Write($"{array[i]} ");
+//         }
+//     }
+
+// Print(member);
+
+// long num1 = 15241383936;
+// double num3 = Math.Sqrt(num1);
+// double num4 = Math.Pow(num3+1, 2);
+// long num2 = ToLong.Parse(num4);
+// System.Console.WriteLine(num2);
+    // long num = 1423281309009358681;
+    // long num2 = 0;
+    // double num1 = Math.Sqrt(num);
+    // long num3 = Convert.ToInt64(num1);
+    // double num5 = Math.Pow(num1+1, 2);
+    // long num6 = Convert.ToInt64(num5);
+    // System.Console.WriteLine(num6);
+    // System.Console.WriteLine(num5);
+    // if (num1 == num3)
+    //   {
+    //   num2 = Convert.ToInt64(Math.Pow(num1+1, 2));
+    //    System.Console.WriteLine(num2);
+    //   }
+    // else 
+    //   {
+    //   System.Console.WriteLine(-1);
+    //   }
+
+// double vIn = 5.4;
+// long vOut = Convert.ToInt64(vIn);
+// // 15241630849
+// System.Console.WriteLine(vOut);
