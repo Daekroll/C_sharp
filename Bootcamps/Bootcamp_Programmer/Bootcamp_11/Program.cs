fa2185ed70e4f2b789d36a1ae8dd5764e4e43ad2
igor <vavilonskiy99@mail.ru>
2023-11-29 14:23:14 +0500

    буткемп 11

---
+++ Bootcamps/Bootcamp_Programmer/Bootcamp_11/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Diagnostics;
+
+const int N = 1000; //размер матрицы
+const int THREADS_NUMBER = 10;
+
+int[,] serialMulRes = new int[N,N]; // результат умножения матриц в однопотке
+int[,] threadMulRes = new int[N,N]; // результат параллельного умножения матриц
+
+int[,] firstMatrix = MatrixGenerator(N, N);
+int[,] secondMatrix = MatrixGenerator(N, N);
+
+Stopwatch sw = new Stopwatch();
+sw.Start();
+SerialMatrixMul(firstMatrix, secondMatrix);
+sw.Stop();
+Console.WriteLine($"SerialMatrixMul: {sw.ElapsedMilliseconds}");
+
+sw.Reset();
+sw.Start();
+PrepareParallelMatrixMul(firstMatrix, secondMatrix);
+sw.Stop();
+Console.WriteLine($"PrepareParallelMatrixMul: {sw.ElapsedMilliseconds}");
+
+Console.WriteLine(EqualityMatrix(serialMulRes, threadMulRes));
+
+int[,] MatrixGenerator (int rows, int columns)
+{
+    Random _rand = new Random();
+    int[,] res = new int[rows, columns];
+    for (int i = 0; i < res.GetLength(0); i++)
+    {
+        for (int j = 0; j < res.GetLength(1); j++)
+        {
+            res[i,j] = _rand.Next(-100, 100);
+        }
+    }
+    return res;
+}
+
+void SerialMatrixMul(int[,] a, int[,] b)
+{
+    if (a.GetLength(1) != b.GetLength(0)) throw new Exception("Нельзя умножать такие матрицы");
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < b.GetLength(1); j++)
+        {
+            for (int k = 0; k < b.GetLength(0); k++)
+            {
+                serialMulRes[i,j] += a[i,k]*b[k, j];
+            }
+        }
+    }
+
+}
+
+void PrepareParallelMatrixMul(int[,] a, int[,] b)
+{
+    if (a.GetLength(1) != b.GetLength(0)) throw new Exception("Нельзя умножать такие матрицы");
+    int eachThreadCalc = N / THREADS_NUMBER;
+    var threadsList = new List<Thread>();
+    for (int i = 0; i < THREADS_NUMBER; i++)
+    {
+        int startPos = i * eachThreadCalc;
+        int endPos = (i+1) * eachThreadCalc;
+        // если поток последний
+        if (i == THREADS_NUMBER-1) endPos = N;
+        threadsList.Add(new Thread(() => ParallelMatrixMul(a, b, startPos, endPos)));
+        threadsList[i].Start();
+    }
+    for (int i = 0; i < THREADS_NUMBER; i++)
+    {
+        threadsList[i].Join();
+    }
+}
+
+void ParallelMatrixMul(int[,] a, int[,] b, int startPos, int endPos)
+{
+    for (int i = startPos; i < endPos; i++)
+    {
+        for (int j = 0; j < b.GetLength(1); j++)
+        {
+            for (int k = 0; k < b.GetLength(0); k++)
+            {
+                threadMulRes[i,j] += a[i,k]*b[k, j];
+            }
+        }
+    }
+}
+
+bool EqualityMatrix (int[,] firstMatrix, int[,] secondMatrix)
+{
+    bool res = true;
+
+    for (int i = 0; i < firstMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < firstMatrix.GetLength(1); j++)
+        {
+            res = res && (firstMatrix[i,j] == secondMatrix[i,j]);
+        }
+    }
+    return res;
+}
--- DZ/DZ_6/Program.cs
+++ DZ/DZ_6/Program.cs
@@ -24,6 +24,7 @@
 // int m = Convert.ToInt32(Console.ReadLine()); // Считываем число из терминала
 // System.Console.WriteLine("Введите число"); // Запрашиваем число у пользователся
 // int n = Convert.ToInt32(Console.ReadLine()); // Считываем число из терминала
+// int i = 0;
 // if (m>n) //Первое введенное число больше
 // {
 //     Console.WriteLine(SummNumbers(n, m));
@@ -45,10 +46,38 @@
 //     return (m + SummNumbers(m+1, n));
 // }
 
+// if (m>n) //Первое введенное число больше
+// {
+//     SummNumbers(n, m, i);
+// }
+// else if (n>m) //Второе введенное число больше
+// {
+//     SummNumbers(m, n, i);
+// }
+// else //Введенные числа равны
+// {
+//     Console.WriteLine("Числа равны");
+// }
+
+// void SummNumbers(int m, int n, int i)
+// {
+//     i+=m;
+//     if (m == n)
+//     {
+//         Console.WriteLine(i);
+//     }
+//     else
+//     {
+        
+//         SummNumbers(m+1, n, i);
+        
+//     } 
+// }
 
 
 // Задача 68: Напишите программу вычисления функции Аккермана
 // с помощью рекурсии. Даны два неотрицательных числа m и n.
+
 
 // System.Console.WriteLine("Введите число"); // Запрашиваем число у пользователся
 // int m = Convert.ToInt32(Console.ReadLine()); // Считываем число из терминала
